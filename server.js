ab1572f9634a95724c27282fbd439bba76bae370
SamSSY <sam7822567@gmail.com>
2015-11-24 20:42:13 +0800

    1124/2044

--- server.js
+++ server.js
@@ -45,56 +45,6 @@ io.sockets.on('connection', function (socket) {
 	    });
     });
 
-	/*	// when the client emits 'add user', this listens and executes
-	socket.on('add user', function (username) {
-		// we store the username in the socket session for this client
-		socket.username = username;
-		// add the client's username to the global list
-		usernames[username] = username;
-		++numUsers;
-		addedUser = true;
-		socket.emit('login', {
-		    numUsers: numUsers
-		});
-		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('user joined', {
-		    username: socket.username,
-		    numUsers: numUsers
-		});
-	});
-
-	// when the client emits 'typing', we broadcast it to others
-	socket.on('typing', function () {
-		socket.broadcast.emit('typing', {
-		    username: socket.username
-		});
-	});
-
-	// when the client emits 'stop typing', we broadcast it to others
-	socket.on('stop typing', function () {
-		socket.broadcast.emit('stop typing', {
-		    username: socket.username
-		});
-	});
-
-
-	  // when the user disconnects.. perform this
-	socket.on('disconnect', function () {
-		// remove the username from global usernames list
-		if (addedUser) {
-			delete usernames[socket.username];
-			--numUsers;
-
-			// echo globally that this client has left
-			socket.broadcast.emit('user left', {
-				username: socket.username,
-				numUsers: numUsers
-			});
-		}
-	});
-
-	*/
-
 });	
 
 
